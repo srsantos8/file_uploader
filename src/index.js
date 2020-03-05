aca0a7778c35a025b06f8575beb8f19fc5854522
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-03-05 20:34:49 -0300

    configuração do mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 const routes = require("express").Router();
 const multer = require("multer");
-
 const multerConfig = require("./config/multer");
 
-routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
-  console.log(req.file);
-  return res.json({
-    hello: "Ramz"
+const Post = require("./models/Post");
+
+routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const { originalname: name, size, filename: key } = req.file;
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url: ""
   });
+  return res.json(post);
 });
 
 module.exports = routes;
