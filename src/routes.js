0403364aa56d08806b748d7567952b89c52078af
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-03-05 19:28:52 -0300

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const routes = require("express").Router();
+const multer = require("multer");
+
+const multerConfig = require("./config/multer");
+
+routes.post("/posts", multer(multerConfig).single("file"), (req, res) => {
+  console.log(req.file);
+  return res.json({
+    hello: "Ramz"
+  });
+});
+
+module.exports = routes;
